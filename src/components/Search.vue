f4cbadd485c4b809050d0dfc41d0638f023c2a66
Kaylee Bunce <kaybunce@hotmail.com>
2019-02-25 12:30:03 -0700

    Add styling to input placeholder

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -24,10 +24,16 @@ export default {
   .search-input {
     width: 80%;
     min-width: 200px;
-    height: 35px;
+    height: 40px;
     border-radius: 10px;
     border: none;
+    padding: 0 0 0 10px;
+    font-size: 20px;
     background-image: linear-gradient(to bottom, rgba(255,252,252,1) 0%,rgba(229,235,238,1) 81%,rgba(215,222,227,1) 100%,rgba(245,247,249,1) 100%,rgba(215,222,227,1) 101%);
+  }
+
+  .search-input::placeholder {
+    font-size: 20px;
   }
 </style>
   
