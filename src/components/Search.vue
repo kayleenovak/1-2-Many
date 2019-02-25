6af228e9610878d6d2d3dd8535b4a4a6946f8597
Kaylee Bunce <kaybunce@hotmail.com>
2019-02-25 12:59:51 -0700

    Add styling for search button

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -3,7 +3,7 @@
     <h1>1-2-Many</h1>
     <form>
       <input class='search-input' placeholder='Find a synonym for...' />
-      <button class='search-btn'>Search</button>
+      <button class='search-btn'>Search</i></button>
     </form>
   </section>
 </template>
@@ -34,6 +34,36 @@ export default {
 
   .search-input::placeholder {
     font-size: 20px;
+  }
+
+  .search-btn {
+    background-image: linear-gradient(top, #f4f1ee, #fff);
+    border-radius: 25px;
+    box-shadow: 0px 8px 10px 0px rgba(0, 0, 0, .3), inset 0px 4px 1px 1px white, inset 0px -3px 1px 1px rgba(204,198,197,.5);
+    height: 40px;
+    margin: 0 30px 30px 0;
+    position: relative;
+    width: 100px;			
+    transition: all .1s linear;
+    font-size: 15px;
+    margin-top: 10px;
+  }
+
+  .search-btn:after{
+    color:#e9e6e4;
+    content: "";
+    display: block;
+    font-size: 30px;
+    height: 30px;
+    text-decoration: none;
+    text-shadow: 0px -1px 1px #bdb5b4, 1px 1px 1px white;
+    position: absolute;
+    width: 60px;
+  }
+
+  .search-btn:hover{
+    background-image: linear-gradient(top, #fff, #f4f1ee);
+    color:#0088cc;
   }
 </style>
   
