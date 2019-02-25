b68ce78db7a93824570caf8ed2e887a8675ed338
Kaylee Bunce <kaybunce@hotmail.com>
2019-02-25 13:09:34 -0700

    Add styling to word name and definition

--- src/components/Word.vue
+++ src/components/Word.vue
@@ -1,16 +1,10 @@
 <template>
   <section class='word-container'>
-    <h1>umpire</h1>
-    <p>a person who impartially decides or resolves a dispute or controversy</p>
-    <h2>Synonyms for umpire</h2>
-    <Synonym />
-    <Synonym />
-    <Synonym />
-    <Synonym />
-    <Synonym />
-    <Synonym />
-    <h2>Example secntences for umpire</h2>
-    <p>usually acts as {it}umpire{/it} in the all-too-frequent squabbles between the two other roommates</p>
+    <h1 class='word-name'>umpire</h1>
+    <p class='word-definition'>a person who impartially decides or resolves a dispute or controversy</p>
+    <h2 class='synonyms-heading'>Synonyms for umpire</h2>
+    <h2 class='example-heading'>Example secntences for umpire</h2>
+    <p class='example-sentence'>usually acts as {it}umpire{/it} in the all-too-frequent squabbles between the two other roommates</p>
   </section>
 </template>
 
@@ -25,18 +19,21 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .word-container {
+    text-align: left;
+    margin-left: 40px;
+  }
+
+  .word-name {
+    font-size: 40px;
+    font-weight: bold;
+    margin-bottom: 7px;
+  }
+
+  .word-definition {
+    margin-top: 0;
+    color: gray;
+  }
+
+  
 </style>
