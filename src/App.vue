eb73c7b34810a0ab90bbc90b2747411ab4472601
Kaylee Bunce <kaybunce@hotmail.com>
2019-02-26 20:08:00 -0700

    Add loading and error messages

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <div id="app">
     <Search v-on:getWord='getWord' />
     <Word v-if='wordDetails.name' v-bind:wordDetails='wordDetails' v-bind:synonyms='synonyms' v-on:getWord='getWord'/>
+    <h2 v-else-if='loading'>{{loading}}</h2>        
+    <h2 v-else-if='error'>{{error}}</h2>    
     <h2 v-else>Please search for a word</h2>
   </div>
 </template>
@@ -24,23 +26,33 @@
           definition: '',
           sentence: []
         },
-        synonyms: []
+        synonyms: [],
+        error: '',
+        loading: ''
       }
     },
     methods: {
       getWord: function(word) {
-        console.log(word)
+          this.wordDetails.name = ''
+          this.error = ''
         if(word) {
+          this.loading = 'Loading...Please Wait'
           fetch(`https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${word}?key=${APIKey}`)
             .then(response => response.json())
-            .then(wordInfo => this.wordCleaner(wordInfo))      
+            .then(wordInfo => this.wordCleaner(wordInfo))
+            .catch(err => this.error = 'Error')      
         }
       },
       wordCleaner: function(wordInfo) {
-        this.wordDetails.name = wordInfo[0].meta.id
-        this.wordDetails.definition = wordInfo[0].shortdef[0]
-        this.wordDetails.sentence = wordInfo[0].def[0].sseq[0][0][1].dt[1][1][0].t.split('{it}').join('').split('{/it}').join()
-        this.synonyms = wordInfo[0].def[0].sseq[0][0][1].syn_list[0].map(word => word.wd)
+        this.loading = ''
+        if (wordInfo[0].meta) {
+          this.wordDetails.name = wordInfo[0].meta.id
+          this.wordDetails.definition = wordInfo[0].shortdef[0]
+          this.wordDetails.sentence = wordInfo[0].def[0].sseq[0][0][1].dt[1][1][0].t.split('{it}').join('').split('{/it}').join()
+          this.synonyms = wordInfo[0].def[0].sseq[0][0][1].syn_list[0].map(word => word.wd)
+        } else {
+          this.error = 'There are no synonyms for that word, please search for another'
+        }
       }
     },
     beforeMount() {
